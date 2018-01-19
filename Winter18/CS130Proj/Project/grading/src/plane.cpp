12a0b04b5e4a6a942b7f2fcf4c46f408cd80af75
Juan <jruiz020@bolt.cs.ucr.edu>
2018-01-18 21:39:44 -0800

    Added basic plane intersections

--- Winter18/CS130Proj/Project/grading/src/plane.cpp
+++ Winter18/CS130Proj/Project/grading/src/plane.cpp
@@ -10,6 +10,15 @@ bool Plane::
 Intersection(const Ray& ray, std::vector<Hit>& hits) const
 {
     // TODO
+    double denominator = dot(normal, ray.direction);
+    if (denominator) {
+       Hit hit;
+       double t1 = dot((x1- ray.endpoint),normal)/denominator; 
+       hit.t = t1;
+       hit.ray_exiting = false;
+       hits.push_back(hit);
+       return true;
+    }
     return false;
 }
 
