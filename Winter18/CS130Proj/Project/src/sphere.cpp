a8a17e8116831c05d9807aad97d8b164511ae5b1
Juan <jruiz020@bolt.cs.ucr.edu>
2018-01-26 12:53:30 -0800

    Passsed most tests

--- Winter18/CS130Proj/Project/src/sphere.cpp
+++ Winter18/CS130Proj/Project/src/sphere.cpp
@@ -8,36 +8,36 @@ bool Sphere::Intersection(const Ray& ray, std::vector<Hit>& hits) const
     // TODO
     vec3 u = ray.direction;
     vec3 v = ray.endpoint - center;
-    double disc = (4 * dot(u,v) * dot(u,v))-4 * (dot(v,v) - radius * radius);
+    double disc = (4 * dot(u,v) * dot(u,v))- 4 * (dot(v,v) - radius * radius);
     if (disc >= 0) {               // there is an intersection
-       Hit h,h2;
+       Hit h1,h2;
        double t;
        double a = 1;
        double b =  2 * dot(v,u);
        double c = dot(v,v) - radius*radius;
 
-       double t1 = (-b-sqrt(disc))/(2*a*c);
-       double t2 = (-b+sqrt(disc))/(2*a*c);
 
        if (disc==0) {              // one intersection
           t = -b/(2*a*c);   
-          h.t = t;
-          h.ray_exiting = false;
-          hits.push_back(h);
+          h1.t = t;
+          h1.ray_exiting = false;
+          hits.push_back(h1);
        }       
        else {                     // two intersections
+          double t1 = (-b-sqrt(disc))/(2*a);
+          double t2 = (-b+sqrt(disc))/(2*a);
           if (t1>0) {
-             h.t = t1;
-             h.ray_exiting = false;
+             h1.t = t1;
+             h1.ray_exiting = false;
              h2.t = t2;
              h2.ray_exiting = true;
-             hits.push_back(h);
+             hits.push_back(h1);
              hits.push_back(h2);
           }
           else if (t2>0){         
-             h.t = t2;
-             h.ray_exiting = true;
-             hits.push_back(h);
+             h1.t = t2;
+             h1.ray_exiting = true;
+             hits.push_back(h1);
           }
           else {                  // no solutions
              return false;
