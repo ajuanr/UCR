53c347f166b5bce39fe944a9754d8948506deb86
Juan <jruiz020@bolt.cs.ucr.edu>
2018-01-26 14:34:35 -0800

    Score of 34

--- Winter18/CS130Proj/Project/src/phong_shader.cpp
+++ Winter18/CS130Proj/Project/src/phong_shader.cpp
@@ -17,22 +17,23 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
                           // ray pointing in : ray pointing out
     vec3 N = (is_exiting) ? -same_side_normal : same_side_normal;
     
-    vec3 C =  (world.camera.position - intersection_point).normalized();        // camera position
+//    vec3 C =  (world.camera.position - intersection_point).normalized();        // camera position
+    vec3 C =  (ray.endpoint - intersection_point).normalized();        // camera position
 
+    // ambient color
+    ambient = world.ambient_color * world.ambient_intensity * color_ambient;
+    color += ambient;
+
+    //if (world.enable_shadows) return color;
     for (auto light = world.lights.begin(); light != world.lights.end(); ++light) {
-       // ambient color
-       ambient = world.ambient_color*world.ambient_intensity*color_ambient;
-
        // Light source stuff
        vec3 l = ((*light)->position - intersection_point); // vector towards light
        vec3 L = l.normalized();
-         
+
        // Shadow ray stuff
        Ray shadow_ray(intersection_point, L);
        Hit hit;
        Object* closestObject = world.Closest_Intersection(shadow_ray,hit); 
-//     if (!closestObject) { 
-
           ////// diffuse color
           double I_d = std::max(dot(L,N),0.0);                    // diffusion intensity
           vec3 light_color = (*light)->Emitted_Light(ray)/ l.magnitude_squared();        // light color
@@ -43,11 +44,14 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
           double I_s = std::pow(std::max(dot(R,C),0.0), specular_power);
           specular = light_color *color_specular * I_s ;
        
-          // final color for current light source
-          color += specular+ diffuse;
-//        }
+        if (world.enable_shadows) {
+           if (!closestObject) { 
+             // final color for current light source
+             color += specular + diffuse;
+           }
+        }
+        else
+            color += specular + diffuse;
     }
-    color += ambient;
-
     return color;
 }
