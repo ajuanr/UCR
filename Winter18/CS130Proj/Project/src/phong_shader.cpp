3552cc3178bfe164b800c06f41198f37ce5df318
Juan <jruiz020@bolt.cs.ucr.edu>
2018-01-26 19:46:14 -0800

    Working on test 21

--- Winter18/CS130Proj/Project/src/phong_shader.cpp
+++ Winter18/CS130Proj/Project/src/phong_shader.cpp
@@ -22,7 +22,6 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 
     // ambient color
     ambient = world.ambient_color * world.ambient_intensity * color_ambient;
-    color += ambient;
 
     //if (world.enable_shadows) return color;
     for (auto light = world.lights.begin(); light != world.lights.end(); ++light) {
@@ -30,28 +29,36 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
        vec3 l = ((*light)->position - intersection_point); // vector towards light
        vec3 L = l.normalized();
 
+       ////// diffuse color
+       double I_d = std::max(dot(L,N),0.0);                    // diffusion intensity
+       vec3 light_color = (*light)->Emitted_Light(ray)/ l.magnitude_squared();        // light color
+       diffuse = light_color * I_d * color_diffuse;
+
+       ////// Specular color
+       vec3 R = ((2 * dot(L,N)) * N - L).normalized();                        // reflected vector
+       double I_s = std::pow(std::max(dot(R,C),0.0), specular_power);
+       specular = light_color *color_specular * I_s ;
+       
        // Shadow ray stuff
        Ray shadow_ray(intersection_point, L);
        Hit hit;
        Object* closestObject = world.Closest_Intersection(shadow_ray,hit); 
-          ////// diffuse color
-          double I_d = std::max(dot(L,N),0.0);                    // diffusion intensity
-          vec3 light_color = (*light)->Emitted_Light(ray)/ l.magnitude_squared();        // light color
-          diffuse = light_color * I_d * color_diffuse;
-
-          ////// Specular color
-          vec3 R = ((2 * dot(L,N)) * N - L).normalized();                        // reflected vector
-          double I_s = std::pow(std::max(dot(R,C),0.0), specular_power);
-          specular = light_color *color_specular * I_s ;
        
-        if (world.enable_shadows) {
-           if (!closestObject) { 
-             // final color for current light source
-             color += specular + diffuse;
-           }
-        }
-        else
-            color += specular + diffuse;
+       double closestIntersectionDistance = shadow_ray.Point(hit.t).magnitude();
+
+       if (world.enable_shadows) {                           // shadows enabled
+          if (closestObject) {                               // intersected an object
+            if (closestIntersectionDistance > l.magnitude()) 
+               continue;                                     // object was behind light 
+          }
+            if (!closestObject){                             // shadow ray did not hit object
+                  color += specular + diffuse;               // add components
+            }
+       }
+       else {                                               // shadows not enabled
+               color += specular + diffuse;                  // add the specular and diffuse
+       }
     }
+    color += ambient;
     return color;
 }
